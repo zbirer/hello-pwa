a9957443a02eb6b924523612944d00d2baed5a9c
Ziv Birer <zbirer@gmail.com>
2021-06-04 12:24:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+    navigator.serviceWorker.register('./sw.js');
   }
 
   if (window.matchMedia('(display-mode: standalone)').matches) {
    console.log('display-mode is standalone');
-   //document.getElementById(elementID).innerHTML = "Good!";
+   document.getElementById("title").innerHTML = "Good!";
   } else {
     console.log('display-mode is other');
-    //document.getElementById(elementID).innerHTML = "Please install app";
+    document.getElementById("title").innerHTML = "Please install app";
   }
-  //console.log("redirect ...");
-  //window.location.replace("https://educareccme.wixsite.com/application");
+  
+  //  console.log("redirect ...");
+  //  window.location.replace("https://educareccme.wixsite.com/application");
 }
